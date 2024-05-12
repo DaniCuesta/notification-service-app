92f4831265d3eb7427b74f275b8d0763007a7126
DanielCuestam <DanielCuestam>
2024-05-12 14:38:28 +0200

    app yaml modified

---
+++ src/main/java/com/example/notificationservice/KafkaProperties.java
@@ -0,0 +1,7 @@
+package kafka.examples;
+
+public class KafkaProperties {
+    public static final String BOOTSTRAP_SERVERS = "10.102.8.124:9092";
+
+    private KafkaProperties() {}
+}
--- src/main/java/com/example/notificationservice/NotificationApplication.java
+++ src/main/java/com/example/notificationservice/NotificationApplication.java
@@ -1,6 +1,6 @@
 package com.example.notificationservice;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -8,14 +8,11 @@ import org.springframework.kafka.annotation.KafkaListener;
 @SpringBootApplication
 public class NotificationApplication {
 
-    @Value("${KAFKA_BOOTSTRAP_SERVERS}")
-    private String kafkaBootstrapServers;
-
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
 
-    @KafkaListener(id = "myGroup", topics = "order-topic", groupId = "notification-consumer-group")
+    @KafkaListener(id = "myGroup", topics = "order-topic")
     public void listen(String in) {
         System.out.println("Received message: " + in);
     }
